fa21c62fe151506888d50d9b4d6b5c60ae74f89c
jyp2270 <jyp2270@naver.com>
2025-08-19 18:23:47 +0900

    로그아웃

--- src/main/java/com/corpIns/config/SecurityConfig.java
+++ src/main/java/com/corpIns/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .logout(logout -> logout.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
